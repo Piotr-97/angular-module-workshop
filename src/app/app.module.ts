7bf9c8eddbd3b9b12aac8bcdfa6d7cd3796572e8
Piotr Karolak <karolak.piotr.012@gmail.com>
2023-07-28 10:06:28 +0200

    exercise done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AComponent } from './my-module/a/a.component';
+import { BComponent } from './my-module/b/b.component';
+import { MyModuleModule } from './my-module/my-module.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+   MyModuleModule,
     BrowserModule
   ],
   providers: [],
